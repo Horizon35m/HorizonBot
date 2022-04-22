e9d937eb6aad8a9de8c19a305a6bf15d35154a21
Horizon35m <92321696+Horizon35m@users.noreply.github.com>
2022-04-22 11:53:12 -0400

    welcome

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -16,6 +19,28 @@ client.on("messageCreate", (message) => {
     if (message.content == "hi"){
         message.reply("Hello World!")
     }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "hi Z"){
+        message.reply("Z is a bitch")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "hi Ohmii"){
+        message.reply("Ohmii is a bitch")
+    }
+})
+
+const welcomeChannelId = "872463673241792554"
+
+client.on("guildMemberAdd", async (member) =>{
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content:`<@${member.id}> Welcome to the Server!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
